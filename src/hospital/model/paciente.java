39a438042192e040dd8532bdf23ea85f10c42f57
sofia <sofiagarcia.2021p@gmail.com>
2024-09-25 08:12:16 -0600

    pacientes

---
+++ src/hospital/model/paciente.java
@@ -0,0 +1,114 @@
+package hospital.model;
+
+public class paciente {
+    private String nombre;
+    private String correo;
+    private String telefono;
+    private String direccion;
+    private String fechaNacimiento;
+    private String horarioCita;
+    private String motivoVisita;
+    private String numeroConsultas;
+    private String doctor;
+
+    public paciente(String nombre, String correo, String telefono, String direccion, String fechaNacimiento, String horarioCita, String motivoVisita, String numeroConsultas, String doctor) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.fechaNacimiento = fechaNacimiento;
+        this.horarioCita = horarioCita;
+        this.motivoVisita = motivoVisita;
+        this.numeroConsultas = numeroConsultas;
+        this.doctor = doctor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getHorarioCita() {
+        return horarioCita;
+    }
+
+    public void setHorarioCita(String horarioCita) {
+        this.horarioCita = horarioCita;
+    }
+
+    public String getMotivoVisita() {
+        return motivoVisita;
+    }
+
+    public void setMotivoVisita(String motivoVisita) {
+        this.motivoVisita = motivoVisita;
+    }
+
+    public String getNumeroConsultas() {
+        return numeroConsultas;
+    }
+
+    public void setNumeroConsultas(String numeroConsultas) {
+        this.numeroConsultas = numeroConsultas;
+    }
+
+    public String getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(String doctor) {
+        this.doctor = doctor;
+    }
+
+    @Override
+    public String toString() {
+        return "Pacientes{" +
+                "nombre='" + nombre + '\'' +
+                ", correo='" + correo + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", Direccion='" + direccion + '\'' +
+                ", fechaNacimiento='" + fechaNacimiento + '\'' +
+                ",HorarioCita='" + horarioCita + '\'' +
+                ",MotivoVisita='" + motivoVisita + '\'' +
+                ",NumeroConsultas='" + numeroConsultas + '\'' +
+                ",Doctor='" + doctor + '\'' +
+                '}';
+    }
+}
+
+
